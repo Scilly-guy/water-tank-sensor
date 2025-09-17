49b59c86a03006ecb6ecdd6fedee971b8f106532
Scilly-guy <h_legg@yahoo.com>
2025-09-17 21:12:08 +0100

    rewrite

--- test.py
+++ test.py
@@ -1,40 +1,51 @@
- #!/usr/bin/python
-#encoding:utf-8
+#!/usr/bin/env python3
+# encoding:utf-8
 
-import RPi.GPIO as GPIO                    #Import GPIO library
-import time                                #Import time library
-GPIO.setmode(GPIO.BCM)                     #Set GPIO pin numbering 
+import RPi.GPIO as GPIO
+import time
 
-TRIG = 15                                  #Associate pin 15 to TRIG
-ECHO = 14                                  #Associate pin 14 to Echo
+GPIO.setmode(GPIO.BCM)  # Use BCM pin numbering
 
-print "Distance measurement in progress"
+TRIG = 15
+ECHO = 14
 
-GPIO.setup(TRIG,GPIO.OUT)                  #Set pin as GPIO out
-GPIO.setup(ECHO,GPIO.IN)                   #Set pin as GPIO in
+print("Distance measurement in progress")
 
-while True:
+GPIO.setup(TRIG, GPIO.OUT)
+GPIO.setup(ECHO, GPIO.IN)
 
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
-  print "Waiting For Sensor To Settle"
-  time.sleep(2)                            #Delay of 2 seconds
+try:
+    while True:
+        GPIO.output(TRIG, False)
+        print("Waiting For Sensor To Settle")
+        time.sleep(2)
 
-  GPIO.output(TRIG, True)                  #Set TRIG as HIGH
-  time.sleep(0.00001)                      #Delay of 0.00001 seconds
-  GPIO.output(TRIG, False)                 #Set TRIG as LOW
+        # Send 10us pulse to trigger
+        GPIO.output(TRIG, True)
+        time.sleep(0.00001)
+        GPIO.output(TRIG, False)
 
-  while GPIO.input(ECHO)==0:               #Check if Echo is LOW
-    pulse_start = time.time()              #Time of the last  LOW pulse
+        # Wait for echo start
+        pulse_start = time.time()
+        timeout = pulse_start + 0.04  # 40ms timeout
+        while GPIO.input(ECHO) == 0 and time.time() < timeout:
+            pulse_start = time.time()
 
-  while GPIO.input(ECHO)==1:               #Check whether Echo is HIGH
-    pulse_end = time.time()                #Time of the last HIGH pulse 
+        # Wait for echo end
+        pulse_end = time.time()
+        timeout = pulse_end + 0.04
+        while GPIO.input(ECHO) == 1 and time.time() < timeout:
+            pulse_end = time.time()
 
-  pulse_duration = pulse_end - pulse_start #pulse duration to a variable
+        pulse_duration = pulse_end - pulse_start
+        distance = pulse_duration * 17150
+        distance = round(distance, 2)
 
-  distance = pulse_duration * 17150        #Calculate distance
-  distance = round(distance, 2)            #Round to two decimal points
+        if 2 <= distance <= 400:  # sensor’s reliable range
+            print("Distance:", distance, "cm")
+        else:
+            print("Out Of Range")
 
-  if distance > 20 and distance < 400:     #Is distance within range
-    print "Distance:",distance - 0.5,"cm"  #Distance with calibration
-  else:
-    print "Out Of Range"                   #display out of range
+except KeyboardInterrupt:
+    print("\nMeasurement stopped by User")
+    GPIO.cleanup()
