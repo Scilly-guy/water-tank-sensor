bf7979cf9a36f024850ee23906ea4e76efea4987
Scilly-guy <h_legg@yahoo.com>
2026-01-16 09:59:53 +0000

    print formatting

--- testbme.py
+++ testbme.py
@@ -7,6 +7,6 @@ pascals = sensor.read_pressure()
 hectopascals = pascals / 100
 humidity = sensor.read_humidity()
 
-print 'Temp      = {0:0.3f} deg C'.format(degrees)
-print 'Pressure  = {0:0.2f} hPa'.format(hectopascals)
-print 'Humidity  = {0:0.2f} %'.format(humidity)
+print(f'Temp      = {degrees:0.2f} deg C')
+print(f'Pressure  = {hectopascals:0.2f} hPa')
+print(f'Humidity  = {humidity:0.2f} %')
